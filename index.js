b245a97439657d469e7cf1bddf1763ba4bce4a83
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2020-04-06 19:39:10 -0700

    added .env file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
+require('dotenv').config();
 const server = require('./server.js')
 
-const port = 5000
+const port = process.env.PORT || 5000
 server.listen(port, () => console.log(`Api listenimg on port ${port}`))
